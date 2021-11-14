00d231c919ed41991f600083c7bf22b8fc9f6bb5
Dan <daehan.jung01@gmail.com>
2021-11-15 00:38:44 +0900

    Create BST.java

---
+++ src/BST.java
@@ -0,0 +1,14 @@
+class TreeNode {
+
+	DVD movie;
+	TreeNode leftChild;
+	TreeNode rightChild;
+	
+	public TreeNode(DVD movie) {
+		
+		this.movie = movie;
+		leftChild = null;
+		rightChild = null;
+		
+	}
+}
